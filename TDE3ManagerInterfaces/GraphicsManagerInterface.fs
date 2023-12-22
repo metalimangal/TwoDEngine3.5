02d4544404cb46c27dc461d1af1adf16cb658a60
professorK <jeffpk@gmail.com>
2023-12-22 16:19:05 -0500

    cleaned up RDnerGraph and added update call
    cleaned up RDnerGraph and added update call.

--- TDE3ManagerInterfaces/GraphicsManagerInterface.fs
+++ TDE3ManagerInterfaces/GraphicsManagerInterface.fs
@@ -28,19 +28,12 @@ and GraphicsManager =
     abstract GraphicsListeners : GraphicsListener list with get, set
     abstract ScreenSize : Vector
     abstract LoadImage : Stream -> Image
-    abstract PushClip : Rectangle -> unit
-    abstract PopClip : unit -> Rectangle option
-    abstract PushTransform : Transform -> unit
-    abstract PopTransform : unit -> Transform option
-    abstract PeekTransform : unit -> Transform option
-    abstract PushMultTransform: Transform -> unit
     abstract DrawImage : Image -> unit
     abstract Start : (GraphicsManager -> unit) -> unit
     abstract Start : unit -> unit
     abstract IdentityTransform : Transform with get
     abstract RotationTransform : float32 -> Transform
     abstract TranslationTransform : float32-> float32 -> Transform
-    
     abstract ScaleTransform : float32-> float32 -> Transform
 
     
