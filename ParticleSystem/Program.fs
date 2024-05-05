79b8b8944177dd5d53c0ae2c1cd752456269ed5a
metali_mangal.purdue <mangalm@purdue.edu>
2024-05-04 20:53:00 -0400

    Added emission rate
    - Unity behaviour wise emission rate added

--- ParticleSystem/Program.fs
+++ ParticleSystem/Program.fs
@@ -83,8 +83,9 @@ let main argv =
 
     // Emit particles once, if you only want a single burst
     let count = 1
-    
     let position = Vector2f(400.0f, 300.f)
+    let emissionRate = 1.0f  // particles per second
+    let mutable emissionAccumulator = 0.0f
     
 
     // Start the main loop
@@ -95,16 +96,23 @@ let main argv =
         
         let color = Color(byte (random.Next(256)), byte (random.Next(256)), byte (random.Next(256)), 255uy)
         // Calculate elapsed time
-        let elapsed = timer.ElapsedTime.AsSeconds()
         let str = "E:/github/TwoDEngine3.5/TwoDEngine3/Assets/football_small.png"
         //let str = "C:/Users/Asus/Downloads/logo.png"
         //ParticleSystem.emitFromLine(position-(Vector2f(200.0f, 200.0f))) (position+(Vector2f(200.0f, 200.0f))) count color 5.f 3.f 100 20.0 None
-        if elapsed>=1.0f then
-            //ParticleSystem.emitFromPoint(position) count color 1.f 3.33f 360 120.0 (Some str)
-            ParticleSystem.emitFromLine(position-(Vector2f(200.0f, 200.0f))) (position+(Vector2f(200.0f, 200.0f))) count color 5.f 3.f 100 20.0 None
+        let frameTime = timer.Restart().AsSeconds()
+
+        // Calculate the number of particles to emit this frame
+        emissionAccumulator <- emissionAccumulator + (emissionRate * frameTime)
+
+        let particlesToEmit = int emissionAccumulator
+
+        // Emit particles based on the calculated amount
+        if particlesToEmit > 0 then
+            ParticleSystem.emitFromPoint(position) particlesToEmit color 1.f 3.33f 360 120.0 (Some str)
+            emissionAccumulator <- emissionAccumulator - float32 particlesToEmit
+            //ParticleSystem.emitFromLine(position-(Vector2f(200.0f, 200.0f))) (position+(Vector2f(200.0f, 200.0f))) count color 5.f 3.f 100 20.0 None
             // Update particles
 
-            timer.Restart().AsSeconds() |> ignore
             
         ParticleSystem.update(1.f /  60.0f)
 
